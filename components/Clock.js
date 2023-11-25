d8dbde78acdc6c988682ce1bbd398ecc7b851409
Joeward Peralta <joeward_peralta@outlook.com>
2023-11-25 00:16:39 -0500

    Fixed clock reset every 60 seconds

--- components/Clock.js
+++ components/Clock.js
@@ -3,11 +3,7 @@ import styles from "@/styles/Home.module.css";
 
 export default function Clock(props) {
   const [date, setDate] = useState(null);
-  let minutes = 0;
-  let seconds = 0;
-  let hours = 0;
-  let day = "";
-  const days = ["SU", "MO", "TU", "WE", "TH", "FR"];
+  const days = ["SU", "MO", "TU", "WE", "TH", "FR", "SA"];
 
   useEffect(() => {
     // update the date once every second
@@ -21,50 +17,42 @@ export default function Clock(props) {
     };
   }, []);
 
-  // Checks if the time are set, if it is valid, format the time
-  if (date?.getHours() && date?.getMinutes() && date?.getSeconds()) {
-    // Get hours in standard time
-    hours = parseInt(date?.getHours()) - 12;
-    hours = hours < 10 ? "0" + hours : hours;
-
-    // Get minutes
-    minutes =
-      date?.getMinutes() < 10 ? "0" + date?.getMinutes() : date?.getMinutes();
-
-    // Get seconds
-    seconds =
-      date?.getSeconds() < 10 ? "0" + date?.getSeconds() : date?.getSeconds();
-
-    // Get current day
-    day = days[date?.getDay()];
-  }
-
   return (
     <>
       <div className={styles.clock}>
         <div className={styles.item}>
-          <h2>{day}</h2>
+          <h2>{days[date?.getDay()]}</h2>
           <p className={styles.label}>day</p>
         </div>
         <div>
           <p>:</p>
         </div>
         <div className={styles.item}>
-          <h2>{hours}</h2>
+          <h2>
+            {date?.getHours() < 10 ? "0" + date?.getHours() : date?.getHours()}
+          </h2>
           <p className={styles.label}>hours</p>
         </div>
         <div>
           <p>:</p>
         </div>
         <div className={styles.item}>
-          <h2>{minutes}</h2>
+          <h2>
+            {date?.getMinutes() < 10
+              ? "0" + date?.getMinutes()
+              : date?.getMinutes()}
+          </h2>
           <p className={styles.label}>minutes</p>
         </div>
         <div>
           <p>:</p>
         </div>
         <div className={styles.item}>
-          <h2>{seconds}</h2>
+          <h2>
+            {date?.getSeconds() < 10
+              ? "0" + date?.getSeconds()
+              : date?.getSeconds()}
+          </h2>
           <p className={styles.label}>seconds</p>
         </div>
       </div>
